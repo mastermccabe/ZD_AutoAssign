ecdc43cb72e4e69244d131163986360c49804286
onlyexcellence <will@wambl.com>
2019-04-08 12:36:38 -0700

    Initial

--- assets/main.js
+++ assets/main.js
@@ -1,40 +1,95 @@
-$(function () {
-  let client = ZAFClient.init();
-
-  client.invoke('hide');
-  // client.invoke('userFields:groups.hide');
-  // client.invoke('userFields:signature.hide');
-
-  client.get(['ticket.assignee','currentUser']).then(function(e) {
-        if (typeof e['ticket.assignee'].user === "undefined"  && (e['ticket.assignee'].group.id === 360001785154 || e['ticket.assignee'].group.id === 360002693173)){
-
-          client.set('ticket.assignee',{ userId: e.currentUser.id});
-          return true;
-        } else {
-          return true;
-        }
-      });
+$(function(){
 
 
-  client.on('ticket.assignee.user.id.changed', function(data){
-    checkAssignee(client)
+  // Store things like this as a constant
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
+  const GROUP_IDS = [
+    360001785154, // Group Name Here
+    360002693173  // Group Name Here
+  ];
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
+
+
+  // Zendesk Client
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
+  const CLIENT = ZAFClient.init();
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
+
+
+  // Functions
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
+
+  // Invoke
+  // ========================================================
+  CLIENT.invoke('hide');
+  // ========================================================
+
+
+  // Get Ticket Assignee and Current User
+  // ========================================================
+  CLIENT.get(['ticket.assignee.user.id','ticket.assignee.group.id','currentUser']).then(function(data){
+
+    let currentUser = data['currentUser'];
+    let assigneeId = data['ticket.assignee.user.id'];
+    let groupId = data['ticket.assignee.group.id'];
+
+    if (!assigneeId && GROUP_IDS.includes(groupId)){
+      CLIENT.set('ticket.assignee',{ userId: currentUser.id });
+    }
+
   });
+  // Old
+  // client.get(['ticket.assignee','currentUser']).then(function(e) {
+  //   if (typeof e['ticket.assignee'].user === "undefined"  && (e['ticket.assignee'].group.id === 360001785154 || e['ticket.assignee'].group.id === 360002693173)){
+  //
+  //     client.set('ticket.assignee',{ userId: e.currentUser.id});
+  //     return true;
+  //   } else {
+  //     return true;
+  //   }
+  // });
+  // ========================================================
 
-  function checkAssignee(client) {
-    client.get('ticket.assignee.user.id').then(
-      function(data){
 
-        if (Number.isInteger(data['ticket.assignee.user.id'])){
-          client.invoke('enableSave');
-        }
-      }, function(response) {
+  // On Assignee Change
+  // ========================================================
+  CLIENT.on('ticket.assignee.user.id.changed',function(newAssigneeId){
 
-      client.invoke('notify', '<b>Agent Assignment Required</b><p></p><p>All tickets must have a user assigned<p>', 'error', 4000)
-			client.invoke('disableSave')
+    if (newAssigneeId){
+      CLIENT.invoke('enableSave');
+    } else {
+      CLIENT.invoke('notify','<b>Agent Assignment Required</b><p></p><p>All tickets must have a user assigned<p>','error',4000);
+			CLIENT.invoke('disableSave');
+    }
 
-      }
-    )
-  };
+  });
+  // Old
+  // client.on('ticket.assignee.user.id.changed', function(data){
+  //   checkAssignee(client)
+  // });
+  // ========================================================
+
+
+  // Check Assignee
+  // ========================================================
+  // Old
+  // function checkAssignee(client) {
+  //   client.get('ticket.assignee.user.id').then(
+  //     function(data){
+  //
+  //       if (Number.isInteger(data['ticket.assignee.user.id'])){
+  //         client.invoke('enableSave');
+  //       }
+  //     }, function(response) {
+  //
+  //     client.invoke('notify', '<b>Agent Assignment Required</b><p></p><p>All tickets must have a user assigned<p>', 'error', 4000)
+	// 		client.invoke('disableSave')
+  //
+  //     }
+  //   )
+  // };
+  // ========================================================
+  // :-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-:-
 
 
 });
